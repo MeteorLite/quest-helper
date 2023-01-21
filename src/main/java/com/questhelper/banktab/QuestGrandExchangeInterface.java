31b0d9a83cfa7daa2bd1bad30dcc2e83d9b845e9
zeruth <therealnull@gmial.com>
2023-01-20 19:19:03 -0500

    meteor: fix remember bank / upstream fixes

--- src/main/java/com/questhelper/banktab/QuestGrandExchangeInterface.java
+++ src/main/java/com/questhelper/banktab/QuestGrandExchangeInterface.java
@@ -91,7 +91,7 @@ public class QuestGrandExchangeInterface
 			QUEST_BUTTON_SIZE,
 			QUEST_BUTTON_SIZE,
 			QUEST_BUTTON_X, QUEST_BUTTON_Y);
-		questBackgroundWidget.setAction(1, VIEW_TAB);
+		//questBackgroundWidget.setAction(1, VIEW_TAB);
 		questBackgroundWidget.setOnOpListener((JavaScriptCallback) this::handleTagTab);
 
 		questIconWidget = createGraphic("", SpriteID.QUESTS_PAGE_ICON_BLUE_QUESTS, QUEST_BUTTON_SIZE - 6,
